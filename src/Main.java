66161ae7bcf31989dc649c572b8b91e39d7d9f85
sergiotech234 <sergio.herbla.4@educa.jcyl.es>
2026-02-27 10:37:08 +0100

    Ejercicio de repaso

--- src/Main.java
+++ src/Main.java
@@ -4,19 +4,20 @@ public class Main {
     public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     int n = sc.nextInt();
-    int [][] matriz = new int[n][n];
+    int m = sc.nextInt();
+    int [][] matriz = new int[n][m];
 // Pedir los valores al usuario
     System.out.println("Ingrese los 9 valores de la matriz 3x3:");
     for (int fila = 0; fila < n; fila++) {
-        for (int columna = 0; columna < n; columna++) {
+        for (int columna = 0; columna < m; columna++) {
             System.out.print("Elemento [" + fila + "][" + columna + "]: ");
             matriz[fila][columna] = sc.nextInt();
         }
     }
         // Mostrar la matriz en formato tabla
         System.out.println("\nMatriz ingresada:");
-        for (int fila = 0; fila < 3; fila++) {
-            for (int columna = 0; columna < 3; columna++) {
+        for (int fila = 0; fila < n; fila++) {
+            for (int columna = 0; columna < m; columna++) {
                 System.out.print(matriz[fila][columna] + "\t");
             }
             System.out.println();
@@ -25,7 +26,7 @@ public class Main {
         System.out.println("\nSuma de cada fila: ");
     for (int fila = 0; fila < n; fila++) {
         int suma = 0;
-        for (int columna = 0; columna < n; columna++) {
+        for (int columna = 0; columna < m; columna++) {
             suma += matriz[fila][columna];
         }
         System.out.println("Fila "+ fila+ " : "+suma);
@@ -34,8 +35,8 @@ public class Main {
         System.out.println("\nSuma de cada columna: ");
         for (int fila = 0; fila < n; fila++) {
             int suma = 0;
-            for (int columna = 0; columna < n; columna++) {
-                suma += matriz[columna][fila];
+            for (int columna = 0; columna < m; columna++) {
+                suma += matriz[fila][columna];
             }
             System.out.println("columna "+ fila+ " : "+suma);
         }
